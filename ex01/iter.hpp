0c1bea72f8eaf827ac1b970361708c08f7f02c30
Ionut Nicoara <inicoara@f2r4s18.clusters.42paris.fr>
2025-12-03 12:12:15 +0100

    checked ex01 done

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -1,9 +1,10 @@
 #ifndef ITER_HPP
 #define ITER_HPP
 #include <cstddef>
+#include <iostream>
 
 template <class T,  class F >
-void iter(T arr, size_t size, F func)
+void iter(T *arr,  const size_t size, F func)
 {
     for (size_t i = 0; i < size; ++i)
         func(arr[i]);
@@ -14,6 +15,10 @@ void printElement(const T& x) {
     std::cout << x << std::endl;
 }
 
-
+template <typename T>
+void printElements(T& x)
+{
+    std::cout << x << std::endl;
+}
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,20 +1,32 @@
-#include <iostream>
 #include "iter.hpp"
 
 
-void print_what(int a)
+static void print_what(const int a)
 {
     std::cout << a << std::endl;
 }
 
-int main() {
-    int arr[] = {1, 2, 3, 4, 5};
-    iter(arr, 5, print_what);
+static void increment (int &a)
+{
+    a += 1;
+}
 
+
+
+int main(void) {
+    int arr[] = {1, 2, 3, 4, 5};
+    ::iter(arr, 5, print_what);
+    std::cout << "After incrementing:" << "\n";
+    ::iter(arr, 5, increment);
+    ::iter(arr, 5, print_what);
+    std::cout << "-------------------" <<  "\n";
     const std::string strs[] = {"hello", "world"};
-    iter(strs, 2, printElement<std::string>);
+    ::iter(strs, 2, printElement<std::string>);
     
     float flts[] = {1.1, 3.3, 23.3};
-     iter(flts, 3, printElement<float>);
+    ::iter(flts, 3, printElement<float>);
+
+    const   double dbls[] = {1.11, 2.22, 3.33};
+    ::iter(dbls, 3, printElements<const double>);
     return 0;
 }
